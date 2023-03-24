e32f2af088672c55265fc61b387b1b8f44512019
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-03-24 10:00:00 +0300

    fix: migration issues

--- src/main/resources/db/testdata/V99_5__Test_create_courses.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_courses.sql
@@ -1,5 +1,5 @@
 insert into "course"
     (id, name, code, credits, total_hours, teacher_id)
-values ('course1_id', 'Interface web', 'WEB1', 4, 'teacher1_id', 22),
-       ('course2_id', 'Algoritmique', 'PROG1', 6, 'teacher1_id', 25),
-       ('course3_id', 'P.O.O avancée', 'PROG3', 6, 'teacher2_id', 25);
+values ('course1_id', 'Interface web', 'WEB1', 4, 22, 'teacher1_id'),
+       ('course2_id', 'Algoritmique', 'PROG1', 6, 25, 'teacher1_id'),
+       ('course3_id', 'P.O.O avancée', 'PROG3', 6, 25, 'teacher2_id');
