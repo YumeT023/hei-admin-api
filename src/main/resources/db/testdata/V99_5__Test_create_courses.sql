b4ac1ec99ca36af29c1f9d1a953e689ef7f75019
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-03-24 04:31:57 +0300

    feat(security): allowing all request on GET /courses

---
+++ src/main/resources/db/testdata/V99_5__Test_create_courses.sql
@@ -0,0 +1,5 @@
+insert into "course"
+    (id, name, code, credits, total_hours, teacher_id)
+values ('course1_id', 'Interface web', 'WEB1', 4, 'teacher1_id', 22),
+       ('course2_id', 'Algoritmique', 'PROG1', 6, 'teacher1_id', 25),
+       ('course3_id', 'P.O.O avancée', 'PROG3', 6, 'teacher2_id', 25);
